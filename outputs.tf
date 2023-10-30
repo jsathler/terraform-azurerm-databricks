d6d75b00fc12b91c47a6c9a74e95ff029e3ec22d
Juliano Sathler <jsathler@live.com>
2023-10-30 12:37:08 -0300

    Added outputs

---
+++ outputs.tf
@@ -0,0 +1,18 @@
+output "id" {
+  value = azurerm_databricks_workspace.default.id
+}
+
+output "managed_resource_group_id" {
+  value = azurerm_databricks_workspace.default.managed_resource_group_id
+}
+output "name" {
+  value = azurerm_databricks_workspace.default.name
+}
+
+output "workspace_id" {
+  value = azurerm_databricks_workspace.default.workspace_id
+}
+
+output "workspace_url" {
+  value = azurerm_databricks_workspace.default.workspace_url
+}
